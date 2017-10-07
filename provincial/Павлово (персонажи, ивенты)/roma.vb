7d0294d488ac2b0665a800ac50f6a705f49f753b
almapower <detectivekunkun@2ch.hk>
2017-10-07 23:16:03 +0800

    Версия 1.2(1 фикс)

--- provincial/Павлово (персонажи, ивенты)/roma.vb
+++
@@ -1,5 +0,0 @@
-﻿*clr & cla
-minut += 5
-gs'stat'
-gs 'zz_render', 'Рома', 'pavlovo/family/sister/sisterQW/ReksHome/Roma.jpg', 'Рыжеволосый худощавый парень по имени Рома'
-act 'Отойти': gt $loc,$metka
--- provincial/Техлоки/LOCA.vb
+++
@@ -1,5 +0,0 @@
-﻿! проверка подключения модов
-_check_mod_id = 0
-gs 'mod_init','check_mod'
-! загрузка карты
-gs 'zz_map_navigation'
